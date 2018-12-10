a9ec191bd901c53d93a857835baa7db98f0f5c02
Carlos Menaier <c_menaier@hotmail.com>
2018-12-10 15:51:34 -0300

    Invoicer formatting

---
+++ Invoicer/Models/ItemRow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Invoicer.Models
+{
+    public class ItemRow
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Price { get; set; }
+        public string Discount { get; set; }
+        public decimal Total { get; set; }
+
+        public bool HasDiscount
+        {
+            get
+            {
+                return (!string.IsNullOrEmpty(Discount));
+            }
+        }
+
+        public static ItemRow Make(string name, string description, decimal amount, decimal price, decimal total)
+        {
+            return Make(name, description, amount, price, "", total);
+        }
+
+        public static ItemRow Make(string name, string description, decimal amount, decimal price, string discount, decimal total)
+        {
+            return new ItemRow()
+            {
+                Name = name,
+                Description = description,
+                Amount = amount,
+                Price = price,
+                Discount = discount,
+                Total = total,
+            };
+        }
+    }
+}
